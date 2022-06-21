a6b55025f8ef8ac6f258f2a7694883c1e0d5e943
Ananya Hegde <54988307+Ananya2504@users.noreply.github.com>
2022-06-21 21:42:35 +0530

    Create Main.java

---
+++ Prgm1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String args[]) {
+		ContactList cl=new ContactList();
+		cl.viewList();
+		MissedCallList ml=new MissedCallList();
+		ml.viewLog();
+	}
+}
